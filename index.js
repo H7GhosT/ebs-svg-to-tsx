210bd81ebedb308d2d9bb9c5c0737ffda800ef35
H7GhosT <nord.doogee@gmail.com>
2021-09-09 10:03:24 +0300

    Some changes

--- index.js
+++ index.js
@@ -35,17 +35,19 @@ const changeAttributes = (_svgText) => {
   let svgText = _svgText;
   svgText = svgText.replace(/width="[^\"]*"/, `width="1em"`);
   svgText = svgText.replace(/height="[^\"]*"/, `height="1em"`);
+  svgText = svgText.replace(/fill="[^\"]*"/g, `fill={fill}`);
+  svgText = svgText.replace(/stroke="[^\"]*"/g, `stroke={stroke}`);
 
-  ["#788494", "#6AA289", "none"].forEach((c) => {
-    svgText = svgText.replace(
-      new RegExp(`stroke="${c}"`, "g"),
-      `stroke={stroke}`
-    );
-    svgText = svgText.replace(new RegExp(`fill="${c}"`, "g"), `fill={fill}`);
-  });
+  // ["#788494", "#6AA289", "none"].forEach((c) => {
+  //   svgText = svgText.replace(
+  //     new RegExp(`stroke="${c}"`, "g"),
+  //     `stroke={stroke}`
+  //   );
+  //   svgText = svgText.replace(new RegExp(`fill="${c}"`, "g"), `fill={fill}`);
+  // });
   // svgText = svgText.replace(/stroke="#788494"/g, `stroke={stroke}`);
   // svgText = svgText.replace(/fill="#788494"/g, `fill={fill}`);
-  svgText = svgText.replace(/fill={fill}/, `fill="none"`);
+  svgText = svgText.replace("fill={fill}", `fill="none"`);
 
   return svgText;
 };
